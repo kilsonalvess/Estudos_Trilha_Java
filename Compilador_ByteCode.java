00068505ee5f10566187cb212c7b11441d397c8e
Kilson Alves <kilsonalves@hotmail.com>
2025-09-23 10:33:22 -0300

    Compilador Bytecode

---
+++ Compilador_ByteCode.java
@@ -0,0 +1,7 @@
+public class Compilador_ByteCode {
+    public static void main(String[] args) {
+        int idade = 36; //Declarando e inicializando um variavel
+        System.out.println("Kilson tem" + " "+idade+" anos"); //Imprimindo e Concatenando
+        
+    }
+}
