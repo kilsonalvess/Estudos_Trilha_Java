7dfaf159552092f56cc93053e7f1ef94695d1804
Kilson Alves <kilsonalves@hotmail.com>
2025-09-23 10:20:26 -0300

    atualização

--- Variaveis.java
+++ Variaveis.java
@@ -1,4 +1,20 @@
 class Variavel {
+     public static void main(String[] args) {
+        //Declaração e iniacialização de Variáveis 
+        
+        int num1 = 1, num2 = 2 , num3 =3; //declarando e inicializando varias variaveis do mesmo tipo
+        int numero_inteiro = 10; //declarando e inicializando
+        String nome = "Kilson"; //declarando e inicializando
+        Double numero_ = 0.5; //declarando e inicializando
+        int inteiro; //inicializando 
+        float preco; //inicializando
+        char caractere; //inicializando
+
+
+
+        System.out.println(num1); //imprimindo um unico argumento
+        System.out.println(num1+ " " +num2); //imprimindo e concatenando mais de um argumento
+    }
     
 
 
